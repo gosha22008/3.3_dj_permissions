e148e7c174a7733f5bd53b138726b10884244361
gosha22008 <gosha22008@yandex.ru>
2021-12-21 17:45:35 +0300

    initial commit

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,29 @@
+from django_filters import DateFromToRangeFilter
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+from api_with_restrictions.permissions import IsOwnerOrReadOnly
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
+
